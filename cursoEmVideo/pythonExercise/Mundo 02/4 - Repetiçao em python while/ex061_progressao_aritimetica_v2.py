ded3cc26de49a2375258b9851cd24262d0174607
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-14 13:26:13 -0300

    feat(pythonExercise): ex061 arithmetic progression

---
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex061_progressao_aritimetica_v2.py
@@ -0,0 +1,10 @@
+# refaça o ex051, leia o primeiro termo e a razao de um PA, retorne os 10 primeiros termos da progressao
+
+n = int(input('Digite o primeiro termo da PA: ')) #primeiro termo
+r = int(input('Digite a razao da PA: ')) #razao
+d = n + (10 - 1) * r #decimo
+i = n
+
+while i < d + r:
+    print(f'{i}')
+    i += r
