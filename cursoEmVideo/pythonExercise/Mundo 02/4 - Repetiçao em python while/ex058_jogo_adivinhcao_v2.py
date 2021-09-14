b439fed49bdb94558a24efff7c0bcf1bed0ee42d
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-14 09:40:09 -0300

    feat(pythonClass): ex058 guessing game v2

---
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex058_jogo_adivinhcao_v2.py
@@ -0,0 +1,24 @@
+# melhore o desafio 028 onde o computador vai 'pensar' em umnumero entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessarios para vencer
+
+# crie um programa que sorteie um numero e receba um valor do usuario como tentativa de acerto
+from random import randint
+from time import sleep
+
+c = randint(0, 10) #faz o computador "PENSAR"
+s = 0 #stop
+t = 1 #tentativas
+
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20 + '\n')
+
+while s == 0:
+    p = int(input('Em que numero eu pensei? '))  # jogada do player1 para tentar adivinhar
+    print('Processando...')
+    sleep(1)
+    if p == c:
+        print(f'PARABENS! Voce tentou {t}x e conseguiu adivinhar!')
+        s = 1
+    else:
+        print(f'GANHEI! Voce tentou {t}x, eu pensei no numero {c} e nao no {p}!')
+        t += 1
