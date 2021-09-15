99cc92b45e901ab4be74e795716271f4efeed287
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-15 16:30:41 -0300

    fix(pythonExercise): ex062 changing output message

--- cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex062_super_progressao_aritimetica_v3.py
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex062_super_progressao_aritimetica_v3.py
@@ -2,14 +2,20 @@
 # pergunte se o usuario gostaria de ver mais termos
 
 n = int(input('Digite o primeiro termo da PA: ')) #primeiro termo
+n_qt = 0 #quantidade de termos
 r = int(input('Digite a razao da PA: ')) #razao
-rr = 10 #rerun
+rr = 9 #rerun
 d = n + rr * r #decimo
 
 while n < d + r:
     while n < d + r:
-        print(f'{n}')
+        print(f'{n}', end='')
         n += r
-    rr = int(input('Digite a quantidade de termos a mais voce gostaria de visualizar: '))
+        n_qt += 1
+        print(' → ' if n < d + r else ' → PAUSA!', end='')
+
+    rr = int(input('\nQuantos termos voce quer mostrar a mais? '))
     if rr > 0:
-        d = n + rr * r  # decimo
+        d = n + (rr - 1) * r  # decimo
+
+print(f'Progressao finalizada com {n_qt} termos!')
