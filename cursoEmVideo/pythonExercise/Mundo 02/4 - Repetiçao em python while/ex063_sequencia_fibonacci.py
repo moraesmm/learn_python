dc3f73af3425994f3f1e2de7f5b212ecdd486078
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-15 16:37:56 -0300

    fix(pythonExercise): ex063 changing output message

--- cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex063_sequencia_fibonacci.py
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex063_sequencia_fibonacci.py
@@ -1,6 +1,9 @@
 # crie um programa que leia um numero e  retorne os n primeiros elementos de uma sequencia de fibonacci
 
-n = int(input('Digite a quantidade de elementos que voce gostaria de exibir da sequencia Fibonacci: '))
+print('-' * 30)
+print('Sequancia de Fibonacci')
+print('-' * 30)
+n = int(input('Qauntos termos voce quer mostrar? '))
 i = 0
 f = 0 #fibonacci[0]
 f1 = 1 #fibonacci[1]
@@ -8,7 +11,8 @@ f2 = 0 #fibonaci[2]
 
 while i < n:
     f2 = f + f1
-    print(f'{f}')
+    print(f'{f}', end='')
     f = f1
     f1 = f2
     i += 1
+    print(' → ' if i < n else ' → Fim!', end='')
