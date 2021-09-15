e31abf87e46bccac75003f9d38b1125bb94b951d
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-15 16:42:11 -0300

    fix(pythonExercise): ex064 changing output message

--- cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex064_tratando_valores.py
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex064_tratando_valores.py
@@ -4,10 +4,11 @@ r = 0 #resposta do usuario
 q_n = 0 #quantidade de numeros digitados
 s_n = 0 #soma dos numeros digitados
 
-while r != 999:
-    n = float(input('Digite um numero: '))
+n = float(input('Digite um numero [999 para sair]: '))
+
+while n != 999:
     q_n += 1
     s_n += n
-    r = int(input('Digite 999 para sair ou qualquer numero para continuar!'))
+    n = float(input('Digite um numero [999 para sair]: '))
 
 print(f'Voce digitou {q_n} numeros e a soma foi {s_n:.1f}!')
