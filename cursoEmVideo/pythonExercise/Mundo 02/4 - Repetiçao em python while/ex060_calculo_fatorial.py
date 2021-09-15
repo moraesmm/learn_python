672156cb380294abf8ec83204503228e36480d6b
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-15 16:12:05 -0300

    fix(pythonExercise): ex060 changing how to calc factorial

--- cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex060_calculo_fatorial.py
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex060_calculo_fatorial.py
@@ -1,16 +1,21 @@
 # crie um programa que leia um numero qualquer e mostre sua factorial
 
-n1 = int(input('Digite um numero: '))
-n = 1
-f = 0
+from math import factorial
 
-if n1 > 0:
-    while n1 != n:
-        print(f'{f} + ({n1} x {n}) = {f + (n1 * n)}')
-        f = f + (n1 * n)
-        n += 1
-    print(f'O fatorial de {n1} é {f}!')
-elif n1 == 0:
+n = int(input('Digite um numero para calcular seu factorial: '))
+c = n
+ff = 1
+
+if n > 0:
+    f = factorial(n)
+    print(f'Calculando {n}! = ', end='')
+    while c > 0:
+        print(f'{c}', end='')
+        print(' x ' if c > 1 else ' = ', end='')
+        ff *= c
+        c -= 1
+    print(f'{f} or {ff}')
+elif n == 0:
     print(f'O fatorial de 0 é 1!')
 else:
     print('Numero negaivo nao tem numero fatorial!')
