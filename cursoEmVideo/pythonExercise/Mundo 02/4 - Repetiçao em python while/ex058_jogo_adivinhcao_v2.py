b53a85e2b87212597b6e6e3b1f102477db654c4f
MoraesMM <moraes.mmauricio@outlook.com>
2021-09-15 15:41:58 -0300

    feat(pythonClass): ex058 change of approach

--- cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex058_jogo_adivinhcao_v2.py
+++ cursoEmVideo/pythonExercise/Mundo 02/4 - Repetiçao em python while/ex058_jogo_adivinhcao_v2.py
@@ -5,20 +5,20 @@ from random import randint
 from time import sleep
 
 c = randint(0, 10) #faz o computador "PENSAR"
-s = 0 #stop
+s = False #stop
 t = 1 #tentativas
 
 print('-=-' * 20)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
 print('-=-' * 20 + '\n')
 
-while s == 0:
+while not s:
     p = int(input('Em que numero eu pensei? '))  # jogada do player1 para tentar adivinhar
     print('Processando...')
     sleep(1)
     if p == c:
         print(f'PARABENS! Voce tentou {t}x e conseguiu adivinhar!')
-        s = 1
+        s = True
     else:
         print(f'GANHEI! Voce tentou {t}x, eu pensei no numero {c} e nao no {p}!')
         t += 1
